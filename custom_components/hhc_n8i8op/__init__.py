f25cd5384377e4c6956c1ee84e000e030610cd2d
flyingpeter <pedroand@hotmail.com>
2025-04-01 13:54:54 +0100

    Update __init__.py

--- custom_components/hhc_n8i8op/__init__.py
+++ custom_components/hhc_n8i8op/__init__.py
@@ -1,7 +1,7 @@
 DOMAIN = "hhc_n8i8op"
 
 
-def setup(hass, config):
+def setup(hass = homeassistant, config):
     hass.states.set("hello_state.world", "Paulus")
 
     # Return boolean to indicate that initialization was successful.

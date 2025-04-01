dc2bb5ad0631bdf966ca75757798ad2829b96723
flyingpeter <pedroand@hotmail.com>
2025-04-01 11:31:32 +0100

    Update config_flow.py

--- custom_components/hhc_n8i8op/config_flow.py
+++ custom_components/hhc_n8i8op/config_flow.py
@@ -1,40 +1,29 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_HOST
+from homeassistant.helpers import config_validation as cv
 import voluptuous as vol
-from homeassistant import config_entries
-from homeassistant.core import callback
-from . import DOMAIN
 
+@config_entries.HANDLERS.register(DOMAIN)
 class TCPRelayConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for TCP Relay."""
-
-    VERSION = 1  # Make sure to specify a version
+    
+    def __init__(self):
+        self._host = None
 
     async def async_step_user(self, user_input=None):
-        """Step where user inputs the IP address."""
+        """Step where user inputs IP address."""
         errors = {}
 
-        # If user has already input the IP address, create an entry
-        if user_input:
-            return self.async_create_entry(title=user_input["host"], data=user_input)
+        if user_input is not None:
+            self._host = user_input.get(CONF_HOST)
+            # You can perform any additional logic or validation here.
 
-        # Show form to enter the host (IP address)
+            return self.async_create_entry(title=self._host, data=user_input)
+
+        # Schema to request the IP address
         schema = vol.Schema({
-            vol.Required("host"): str  # Ensure host is a required string
+            vol.Required(CONF_HOST): str,
         })
 
         return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
 
-    @staticmethod
-    @callback
-    def async_get_options_flow(entry):
-        """Handle options for the config entry."""
-        return TCPRelayOptionsFlow(entry)
-
-class TCPRelayOptionsFlow(config_entries.OptionsFlow):
-    """Handle device options (if needed)."""
-
-    def __init__(self, entry):
-        self.entry = entry
-
-    async def async_step_init(self, user_input=None):
-        """Handle options flow."""
-        return self.async_show_form(step_id="init", data_schema=vol.Schema({}))
